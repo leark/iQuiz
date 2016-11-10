848484659b9f88ae0b0f12af2383700a44a663f2
Seung Lee <shl7@uw.edu>
2016-11-09 17:12:24 -0800

    added tableview, subjects, settings button

---
+++ iQuiz/iQuizNavViewController.swift
@@ -0,0 +1,48 @@
+//
+//  iQuizNavViewController.swift
+//  iQuiz
+//
+//  Created by iGuest on 11/9/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class iQuizNavViewController: UINavigationController {
+
+    @IBOutlet weak var settings: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func openSettings(_ sender: AnyObject) {
+        let settingsController = UIAlertController(title: "Settings go here", message: "", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        settingsController.addAction(defaultAction)
+        
+        present(settingsController, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iQuiz/iQuizTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  iQuizTableViewCell.swift
+//  iQuiz
+//
+//  Created by iGuest on 11/9/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class iQuizTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var iconView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
